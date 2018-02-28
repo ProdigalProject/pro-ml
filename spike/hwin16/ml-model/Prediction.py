bd98ead1b01df4420d2928b5b436a5da4c9d80e8
Htut Khine Win <hwin16@illinois.edu>
2018-02-28 05:53:45 +0000

    Test manual machine learning model.

--- spike/hwin16/ml-model/Prediction.py
+++ spike/hwin16/ml-model/Prediction.py
@@ -1,12 +1,21 @@
-import numpy as np 
-from datetime import datetime
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 
 # load 
-raw_data = np.loadtxt("apple_20.csv", 
-                       usecols=(1:)
-                       delimiter=",", 
-                       skiprows=1) 
+file_path = "apple_20.csv" 
+raw_data = pd.read_csv(file_path) 
+y = raw_data.close 
+X = raw_data.loc[:, raw_data.columns != "close"] 
+
+# preprocess
+
 
 # train test split data 
-raw_data
+train_X, train_y, test_X, test_y = train_test_split(X, y, test_size=0.3)
+
+# learn model
+model = LinearRegression()
+train_model = model.fit(train_X, train_y) 
 
