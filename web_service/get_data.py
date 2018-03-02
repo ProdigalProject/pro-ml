463533763af759647056d7cdef8edcf4986c247a
jlp3 <jlp3@illinois.edu>
2018-03-02 14:22:59 -0600

    Update get_data.py to be PEP8 compliant

--- web_service/get_data.py
+++ web_service/get_data.py
@@ -1,16 +1,26 @@
 import urllib.request
 import json
 
+
 class Data:
 
     def __init__(self):
-        self._api_key = 'e7MiObHOMV+CJ9/I1lahT8HighwH4jDbmhcc30SgePzhkVl4WEEQ/nCpSqNCCZ2K8YIefHKiCQbzpdVCBHlqhA=='
-        self._url = 'https://ussouthcentral.services.azureml.net/workspaces/5c0fb2c3710c46348125a99f6dd0e1df/services/e20bef5fd25447658e67952606835a4c/execute?api-version=2.0&format=swagger'
+        self._api_key = 'e7MiObHOMV+CJ9/' +
+        'I1lahT8HighwH4jDbmhcc30SgePzhkVl4WEEQ/' +
+        'nCpSqNCCZ2K8YIefHKiCQbzpdVCBHlqhA=='
+        self._url = 'https://ussouthcentral.services.azureml.net/workspaces/' +
+        '5c0fb2c3710c46348125a99f6dd0e1df/services/' +
+        'e20bef5fd25447658e67952606835a4c/' +
+        'execute?api-version=2.0&format=swagger'
 
     def get_data(input):
         body = str.encode(json.dumps(input))
-        url = 'https://ussouthcentral.services.azureml.net/workspaces/5c0fb2c3710c46348125a99f6dd0e1df/services/e20bef5fd25447658e67952606835a4c/execute?api-version=2.0&format=swagger'
-        api_key = 'e7MiObHOMV+CJ9/I1lahT8HighwH4jDbmhcc30SgePzhkVl4WEEQ/nCpSqNCCZ2K8YIefHKiCQbzpdVCBHlqhA=='
+        url = 'https://ussouthcentral.services.azureml.net/workspaces/' +
+        '5c0fb2c3710c46348125a99f6dd0e1df/services/' +
+        'e20bef5fd25447658e67952606835a4c/' +
+        'execute?api-version=2.0&format=swagger'
+        api_key = 'e7MiObHOMV+CJ9/I1lahT8HighwH4jDbmhcc30SgePzhkVl4WEEQ/' +
+        'nCpSqNCCZ2K8YIefHKiCQbzpdVCBHlqhA=='
         headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
         req = urllib.request.Request(url, body, headers)
 
@@ -19,10 +29,10 @@ class Data:
             result = response.read()
             print(result)
         except urllib.error.HTTPError as error:
-             print("The request failed with status code: " + str(error.code))
-             # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-             print(error.info())
-             print(json.loads(error.read().decode("utf8", 'ignore')))
+            print("The request failed with status code: " + str(error.code))
+            print(error.info())
+            print(json.loads(error.read().decode("utf8", 'ignore')))
+
 
 def main():
         data = {
@@ -30,12 +40,12 @@ def main():
                 "Apple_Price_History":
                 [
                     {
-                           'timestamp': "2018-04-14T00:00:00",  
-                           'open': "1",  
-                           'high': "1",  
-                           'low': "1",  
-                           'close': "1",  
-                           'volume': "1",  
+                           'timestamp': "2018-04-14T00:00:00",
+                           'open': "1",
+                           'high': "1",
+                           'low': "1",
+                           'close': "1",
+                           'volume': "1",
                     }
                 ],
             },
