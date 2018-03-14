dfb2c4b12caf94d68d6983fb632348b9f2a2ae02
Htut Khine <hwin16@illinois.edu>
2018-03-14 02:53:03 -0500

    Wrote cron-job for inserting nightly.

--- backend/data_mine/tasks.py
+++ backend/data_mine/tasks.py
@@ -1,8 +1,29 @@
-from __future__ import absolute_import 
-from celery.decorators import task
+from celery.schedules import crontab 
+from backend.celery import app as celery_app 
+from data_mine.InsertTickers import InsertTickers
 
-@task(name="say_hello")
+@celery_app.task(name="say_hello")
 def hello(): 
     return "HELLO" 
 
-hello.delay()
+@celery_app.task(name="insert_ticker_nightly")
+def insert_ticker_nightly(): 
+    ins = InsertTickers()
+    ins.run(1, ['AAPL']) 
+
+@celery_app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs): 
+    # Call task every 10 seconds. 
+    sender.add_periodic_task(
+        2.0,
+        insert_ticker_nightly.s(), 
+        name="insert_ticker") 
+
+    ''' 
+    # Executes crontab 
+    sender.add_periodic_task(
+        crontab(hour=4, minute=39), 
+        insert_ticker_nightly.s(),
+        name="insert_ticker_nightly"
+    ) 
+    '''
