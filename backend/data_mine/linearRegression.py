ab29a2a89f5c0c69b84f1f4b4a9ddb4d343d05a6
gchen46 <gchen46@illinois.edu>
2018-03-15 00:47:04 -0500

    Closing price prediction now uses all three features (open, high, low) when predicting the closing price.

--- backend/data_mine/linearRegression.py
+++ backend/data_mine/linearRegression.py
@@ -1,9 +1,29 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("apple_20.csv")
 
+X = data[["open", "high", "low"]]
+y = data["close"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+pred = model.predict([[163.045, 165.81, 162.88]])
+for i in pred:
+    i = str(float(i))
+    print("Predicted Closing Price: " + "$" + i)
+
+"""
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+print(df)
+"""
+
+"""
 open_price = data["open"]
 closing_price = data["close"]
 x = np.array(open_price).reshape(-1,1)
@@ -16,3 +36,5 @@ pred = model.predict(test_val)
 for i in pred:
 	i = str(float(i))
 	print("Predicted Closing Price: " + "$" + i)
+"""
+
