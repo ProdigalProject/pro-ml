c58dbc2a6a7c588ee46930959414cff0d00e8d0d
Htut Khine <htutkhwin@gmail.com>
2018-03-15 13:46:47 -0500

    Fixed panda issue.

--- backend/data_mine/tests/test_insertTickers.py
+++ backend/data_mine/tests/test_insertTickers.py
@@ -7,10 +7,6 @@ class InsertTickersTestCase(TestCase):
     pdg_link = 'http://prodigal-ml.us-east-2.elasticbeanstalk.com/stocks/'
     it = InsertTickers() 
 
-    def test_get_datelist(self): 
-        date_list = self.it.get_datelist(1)
-        assert(len(date_list) != 0) 
-
     def test_get_custom_ticker(self): 
         ticker = 'AAPL'
         cdata = self.it.get_custom_ticker(ticker)
