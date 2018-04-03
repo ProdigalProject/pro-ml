ddad2da8ef4a59a501338c5396278683b0dfd4fb
Wonwoo Seo <wonwooseo@hotmail.com>
2018-04-03 18:07:59 -0500

    started writing tests

--- backend/stocks/tests.py
+++ backend/stocks/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ViewTestCase(TestCase):
+    def test_get_stock_history(self):
+        response = self.client.get('/stocks/AAPL?ordering=-date&format=json')
+        # json_data = response.json()
+        self.assertEqual(200, response.status_code)
